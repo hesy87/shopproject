cc67dc22054325a92809e7d2098744c19222c4f1
hesy87 <hesam.mahmoodi@gmail.com>
2023-12-07 23:59:04 +0330

    product category selector established

--- app/containers/ProductList.jsx
+++ app/containers/ProductList.jsx
@@ -7,16 +7,22 @@ import Card from "../components/UI/Card/CardProductList";
 import { useDataSelector } from "../store/hooks";
 
 const ProductList = () => {
+  const selectedCategory = useDataSelector((state) => state.category.items);
   const [product, setProduct] = useState([]);
   useEffect(() => {
     const FetchProduct = async () => {
-      const response = await GET(ROUTE_PRODUCTS);
-      setProduct(response);
+      if (selectedCategory === "") {
+        const response = await GET(ROUTE_PRODUCTS);
+        setProduct(response);
+      } else {
+        const response = await GET(ROUTE_PRODUCTS);
+        setProduct(
+          response.filter((item) => item.category === selectedCategory)
+        );
+      }
     };
     FetchProduct();
-  }, []);
-  const selectedCategory = useDataSelector((state) => state.category.items);
-  console.log(selectedCategory);
+  }, [selectedCategory]);
   return (
     <div className="grid grid-cols-3 gap-3 m-10">
       {product.map((item) => (
